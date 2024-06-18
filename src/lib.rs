628cf32e6e9eb2d1ccf7837bce4d046e853eeeeb
hkernbach <heiko@arangodb.com>
2024-06-18 14:20:56 +0200

    init code commit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub mod arangodb;
+pub mod client;
+pub mod input;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
