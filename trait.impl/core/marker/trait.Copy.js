b847284d7c9ee6f771654712577e78022beaa7e6
hkernbach <heiko@arangodb.com>
2024-08-05 17:58:48 +0200

    clear all

--- trait.impl/core/marker/trait.Copy.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"arangors_graph_exporter":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"arangors_graph_exporter/types/info/enum.LoadStrategy.html\" title=\"enum arangors_graph_exporter::types::info::LoadStrategy\">LoadStrategy</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
