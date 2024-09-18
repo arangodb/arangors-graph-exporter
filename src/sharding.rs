e54bf527ff293ec38874fd4237bc565de5559b2d
hkernbach <heiko@arangodb.com>
2024-09-18 11:35:57 +0200

    expose the errors during task join to the user, this caused a lot of debugging time in the past

--- src/sharding.rs
+++ src/sharding.rs
@@ -6,7 +6,7 @@ use crate::request::handle_arangodb_response;
 use crate::types::info::DeploymentType;
 use crate::{errors, DataLoadConfiguration, DatabaseConfiguration};
 use bytes::Bytes;
-use log::{debug, error};
+use log::{debug, error, info};
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
@@ -273,22 +273,31 @@ pub(crate) async fn get_all_shard_data(
                         .bytes()
                         .await
                         .map_err(|e| format!("Error in body: {:?}", e))?;
-                    result_channel_clone
-                        .send(body)
-                        .await
-                        .expect("Could not send to channel!");
+                    match result_channel_clone.send(body).await {
+                        Ok(_) => {
+                            // Successfully sent to the channel
+                            debug!("Successfully sent message to channel.");
+                        }
+                        Err(e) => {
+                            panic!("Failed to send to channel: {:?}", e);
+                        }
+                    }
                 }
             });
         }
     }
     while let Some(res) = task_set.join_next().await {
-        let r = res.unwrap();
-        match r {
-            Ok(_x) => {
+        match res {
+            Ok(Ok(_x)) => {
                 debug!("Got OK result!");
             }
-            Err(msg) => {
-                debug!("Got error result: {}", msg);
+            Ok(Err(err_msg)) => {
+                // This handles errors that occur within the task itself
+                panic!("Task failed with error: {:?}", err_msg);
+            }
+            Err(join_err) => {
+                // This handles errors in the task joining process (e.g., task panicked)
+                panic!("Task join error occurred: {:?}", join_err)
             }
         }
     }
