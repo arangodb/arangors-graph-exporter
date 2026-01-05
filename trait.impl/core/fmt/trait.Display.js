9c9da406b56f6d5cf507022e4f9297131575891a
neunhoef <neunhoef@users.noreply.github.com>
2026-01-05 15:00:51 +0000

    deploy: 5cb396eaa8907949da569d624c24beb1547b8a9b

--- trait.impl/core/fmt/trait.Display.js
+++ trait.impl/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["arangors_graph_exporter",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"arangors_graph_exporter/errors/enum.GraphLoaderError.html\" title=\"enum arangors_graph_exporter::errors::GraphLoaderError\">GraphLoaderError</a>"]]]]);
+    var implementors = Object.fromEntries([["arangors_graph_exporter",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"arangors_graph_exporter/errors/enum.GraphLoaderError.html\" title=\"enum arangors_graph_exporter::errors::GraphLoaderError\">GraphLoaderError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["arangors_graph_exporter",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"arangors_graph_exporter/types/info/enum.LoadStrategy.html\" title=\"enum arangors_graph_exporter::types::info::LoadStrategy\">LoadStrategy</a>"]]]]);
+    var implementors = Object.fromEntries([["arangors_graph_exporter",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"arangors_graph_exporter/types/info/enum.LoadStrategy.html\" title=\"enum arangors_graph_exporter::types::info::LoadStrategy\">LoadStrategy</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
